d25b773110e8767725bd16f0ace7ba05b99e9608
Chineselock <38371576+Chineselock@users.noreply.github.com>
2021-12-07 21:42:22 +0800

    Add files via upload

---
+++ HVDtrain.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import json
+import numpy as np
+import random
+import torch
+import wandb
+import os
+import horovod.torch as hvd
+import torch.distributed as dist
+from torch.utils.data import DataLoader, Dataset 
+from transformers import AdamW, BertForQuestionAnswering, BertTokenizerFast
+from tqdm.auto import tqdm
+import time
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Fix random seed for reproducibility
+def same_seeds(seed):
+	  torch.manual_seed(seed)
+	  if torch.cuda.is_available():
+		    torch.cuda.manual_seed(seed)
+		    torch.cuda.manual_seed_all(seed)
+	  np.random.seed(seed)
+	  random.seed(seed)
+	  torch.backends.cudnn.benchmark = False
+	  torch.backends.cudnn.deterministic = True
+same_seeds(0)
+
+
+
+hvd.init()
+torch.cuda.set_device(hvd.local_rank())
+
+
+
+
+model = BertForQuestionAnswering.from_pretrained("bert-base-chinese").to(device)
+tokenizer = BertTokenizerFast.from_pretrained("bert-base-chinese")
+
+
+
+
+def read_data(file):
+    with open(file, 'r', encoding="utf-8") as reader:
+        data = json.load(reader)
+    return data["questions"], data["paragraphs"]
+
+train_questions, train_paragraphs = read_data("hw7_train.json")
+# Tokenize questions and paragraphs separately
+# 「add_special_tokens」 is set to False since special tokens will be added when tokenized questions and paragraphs are combined in datset __getitem__ 
+
+train_questions_tokenized = tokenizer([train_question["question_text"] for train_question in train_questions], add_special_tokens=False)
+
+train_paragraphs_tokenized = tokenizer(train_paragraphs, add_special_tokens=False)
+
+# You can safely ignore the warning message as tokenized sequences will be futher processed in datset __getitem__ before passing to model
+
+
+
+
+class QA_Dataset(Dataset):
+    def __init__(self, split, questions, tokenized_questions, tokenized_paragraphs):
+        self.split = split
+        self.questions = questions
+        self.tokenized_questions = tokenized_questions
+        self.tokenized_paragraphs = tokenized_paragraphs
+        self.max_question_len = 40
+        self.max_paragraph_len = 150
+        
+        ##### TODO: Change value of doc_stride #####
+        self.doc_stride =  30
+
+        # Input sequence length = [CLS] + question + [SEP] + paragraph + [SEP]
+        self.max_seq_len = 1 + self.max_question_len + 1 + self.max_paragraph_len + 1
+
+    def __len__(self):
+        return len(self.questions)
+
+    def __getitem__(self, idx):
+        question = self.questions[idx]
+        tokenized_question = self.tokenized_questions[idx]
+        tokenized_paragraph = self.tokenized_paragraphs[question["paragraph_id"]]
+
+        ##### TODO: Preprocessing #####
+        # Hint: How to prevent model from learning something it should not learn
+        if self.split == "train":
+            # Convert answer's start/end positions in paragraph_text to start/end positions in tokenized_paragraph  
+            answer_start_token = tokenized_paragraph.char_to_token(question["answer_start"])
+            answer_end_token = tokenized_paragraph.char_to_token(question["answer_end"])
+
+            # A single window is obtained by slicing the portion of paragraph containing the answer
+            #答案在窗口中间，这个规律是不应该被机器学到的，这里随机切入窗口
+            mid = (answer_start_token + answer_end_token) // 2
+            answer_length = answer_end_token - answer_start_token + 1
+            if answer_length // 2 < self.max_paragraph_len - answer_length // 2:
+              rnd = random.randint(answer_length // 2, self.max_paragraph_len - answer_length // 2)
+            else:
+              rnd = self.max_paragraph_len // 2
+            paragraph_start = max(0, min(mid - rnd, len(tokenized_paragraph) - self.max_paragraph_len))
+            paragraph_end = paragraph_start + self.max_paragraph_len
+            
+            # Slice question/paragraph and add special tokens (101: CLS, 102: SEP)
+            input_ids_question = [101] + tokenized_question.ids[:self.max_question_len] + [102] 
+            input_ids_paragraph = tokenized_paragraph.ids[paragraph_start : paragraph_end] + [102]		
+            
+            # Convert answer's start/end positions in tokenized_paragraph to start/end positions in the window  
+            answer_start_token += len(input_ids_question) - paragraph_start
+            answer_end_token += len(input_ids_question) - paragraph_start
+            
+            # Pad sequence and obtain inputs to model 
+            input_ids, token_type_ids, attention_mask = self.padding(input_ids_question, input_ids_paragraph)
+            return torch.tensor(input_ids), torch.tensor(token_type_ids), torch.tensor(attention_mask), answer_start_token, answer_end_token
+
+
+    def padding(self, input_ids_question, input_ids_paragraph):
+        # Pad zeros if sequence length is shorter than max_seq_len
+        padding_len = self.max_seq_len - len(input_ids_question) - len(input_ids_paragraph)
+        # Indices of input sequence tokens in the vocabulary
+        input_ids = input_ids_question + input_ids_paragraph + [0] * padding_len
+        # Segment token indices to indicate first and second portions of the inputs. Indices are selected in [0, 1]
+        token_type_ids = [0] * len(input_ids_question) + [1] * len(input_ids_paragraph) + [0] * padding_len
+        # Mask to avoid performing attention on padding token indices. Mask values selected in [0, 1]
+        attention_mask = [1] * (len(input_ids_question) + len(input_ids_paragraph)) + [0] * padding_len
+        
+        return input_ids, token_type_ids, attention_mask
+
+train_set = QA_Dataset("train", train_questions, train_questions_tokenized, train_paragraphs_tokenized)
+
+train_sampler = torch.utils.data.distributed.DistributedSampler(train_set, num_replicas=hvd.size(), rank=hvd.rank())
+
+train_batch_size = 32
+nw = 4
+# Note: Do NOT change batch size of dev_loader / test_loader !
+# Although batch size=1, it is actually a batch consisting of several windows from the same QA pair
+train_loader = DataLoader(train_set, batch_size=train_batch_size, shuffle=False, pin_memory=True, num_workers=nw, sampler=train_sampler)
+
+
+
+
+def evaluate(data, output):
+    ##### TODO: Postprocessing #####
+    # There is a bug and room for improvement in postprocessing 
+    # Hint: Open your prediction file to see what is wrong 
+    
+    answer = ''
+    max_prob = float('-inf')
+    num_of_windows = data[0].shape[1]
+    
+    for k in range(num_of_windows):
+        # Obtain answer by choosing the most probable start position / end position
+        start_prob, start_index = torch.max(output.start_logits[k], dim=0)
+        end_prob, end_index = torch.max(output.end_logits[k], dim=0)
+        
+        # Probability of answer is calculated as sum of start_prob and end_prob
+        prob = start_prob + end_prob
+        
+        # Replace answer if calculated probability is larger than previous windows
+        if prob > max_prob:
+            max_prob = prob
+            # Convert tokens to chars (e.g. [1920, 7032] --> "大 金")
+            if start_index > end_index:
+              answer = ''
+            else:
+              answer = tokenizer.decode(data[0][0][k][start_index : end_index + 1])
+    
+    # Remove spaces in answer (e.g. "大 金" --> "大金")
+    return answer.replace(' ','')
+
+wandb.init(project="HVD_2M")
+
+num_epoch = 1
+logging_step = 10
+learning_rate = 1e-4
+optimizer = AdamW(model.parameters(), lr=learning_rate)
+optimizer = hvd.DistributedOptimizer(optimizer, named_parameters=model.named_parameters())
+hvd.broadcast_parameters(model.state_dict(), root_rank=0)
+
+model.train()
+
+print("Start Training ...")
+st=time.time()
+
+for epoch in range(num_epoch):
+    step = 1
+    train_loss = train_acc = 0
+    
+    for data in tqdm(train_loader):	
+        # Load all data into GPU
+        data = [i.to(device) for i in data]
+        t1=time.time()
+        # Model inputs: input_ids, token_type_ids, attention_mask, start_positions, end_positions (Note: only "input_ids" is mandatory)
+        # Model outputs: start_logits, end_logits, loss (return when start_positions/end_positions are provided)  
+        output = model(input_ids=data[0], token_type_ids=data[1], attention_mask=data[2], start_positions=data[3], end_positions=data[4])
+
+        # Choose the most probable start position / end position
+        start_index = torch.argmax(output.start_logits, dim=1)
+        end_index = torch.argmax(output.end_logits, dim=1)
+        t2=time.time()
+        
+        
+        output.loss.backward()
+        t3=time.time()
+        
+        optimizer.step()
+        optimizer.zero_grad()
+        t4=time.time()
+        
+        step += 1
+        optimizer.param_groups[0]["lr"] -= learning_rate/1684
+        if step == 50:
+            print()
+            print(f"predict time is: {t2-t1}s")
+            print(f"backward time is: {t3-t2}s")
+            print(f"optimizer time is:{t4-t3}s")
+    
+en=time.time()
+print("End Train.Total Time:",en-st)
